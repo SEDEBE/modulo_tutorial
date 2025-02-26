a9f9f88e888e2e20a2a22c215f0e239d284fd4d2
Sedebe <alejandrocordoba28@gmail.com>
2025-02-26 10:27:28 +0100

    act 2.0

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'summary': 'Modulo de informacion de algunas skins de Counter Strike',
     'author': 'Alejandro Cordoba Perez',
-    'depends': 'base',
+    'depends': ['base'],
     'category': 'Custom',
     'data': [
         'views/skins_views.xml',
